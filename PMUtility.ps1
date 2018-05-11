e69d0ca5990cb7a897c0b52bfc69f35e5f7cbb0d
Nick Dollimount <nickdollimount@nickdollimount.com>
2018-05-11 10:08:42 -0300

    adCmdlets function
    Added function to check for installed AD cmdlets.  If not found, user will be prompted to install the to continue. If found, they will be imported.

--- PMUtility.ps1
+++ PMUtility.ps1
@@ -23,3 +23,24 @@ function getGroups($groupsVar){
 }
 
 getGroups ("Custom Group 1","Custom Group 2","CustomDoesNotExist")
+
+function adCmdlets(){
+    # Check if AD cmdlets are installed and prompt to install them to continue; otherwise import them.
+    if (!(Get-Module -ListAvailable -Name activedirectory)){
+        $response = Read-Host "Active Directory modules are not installed. Do you want to install them now to continue? [Y/N]"
+        switch ($response.ToLower()){
+            "y" {
+                Add-WindowsFeature RSAT-AD-PowerShell
+                Import-Module activedirectory
+            }
+            "n" {
+                Return
+            }
+        }
+    
+    }
+
+    else {
+        Import-Module activedirectory
+    }
+}
