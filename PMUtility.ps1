c143a200c1073bce5cf3b739244c164065bf8157
Nick Dollimount <nickdollimount@nickdollimount.com>
2018-05-11 09:36:31 -0300

    getGroups started
    Started the framework for verifying the groups that are specified by the user.

---
+++ PMUtility.ps1
@@ -0,0 +1,25 @@
+﻿$Groups = @()
+
+function getGroups($groupsVar){
+
+    $groupDN
+
+    $groupsVar | %{
+        $Error.Clear()
+        try{
+            $groupDN = (Get-ADGroup $_).distinguishedName
+        }
+        catch{
+
+        }
+        if (!$Error){
+            $Groups += $groupDN
+        }
+    }
+
+    Write-Output ("The following groups were confirmed:")
+
+    $Groups
+}
+
+getGroups ("Custom Group 1","Custom Group 2","CustomDoesNotExist")
